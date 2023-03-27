91e4bd2ea06ac8d5a6f460839df8c4901e925ae0
Ignas Dev <iberthold248@gmail.com>
2023-03-27 07:36:41 -0700

    Update Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
-*main - check the code
-*
-*Return: Always 0
-*/
-int main(void)
-{
-  int n;
-  n = 402;
-  printf("=%d\n", n);
-  reset_to_98(&n);
-  printf("n=%d\n", n);
-  return (0);
-}
 
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value.
